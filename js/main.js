914675c7cc0cf4e2c191dda19622a6be6e827892
Michael Frank <frank.522@osu.edu>
2021-06-17 19:28:22 -0400

    Created functionality for board generation, drawing, and deletion

--- js/main.js
+++ js/main.js
@@ -1,15 +1,35 @@
 const etch = document.createElement('div');
-etch.setAttribute('id', 'mainDiv');
 
-for(let i=0; i < 16; i++){
-    const row = document.createElement('div');
-    row.setAttribute('id', 'row')
-    for(let j=0; j < 16; j++){
-        const cell = document.createElement('div');
-        cell.setAttribute('id', 'cell');
-        row.appendChild(cell);
+let changeColor = (e) => {
+    if(e.target.getAttribute('class') == 'cell') {
+        e.target.classList.add('clicked');
     }
-    etch.appendChild(row);
-}
+};
 
+let generateBoard = (n) => {
+    for(let i=0; i < n; i++){
+        const row = document.createElement('div');
+        row.setAttribute('class', 'row');
+        for(let j=0; j < n; j++){
+            const cell = document.createElement('div');
+            cell.setAttribute('class', 'cell');
+            row.appendChild(cell);
+            cell.addEventListener('mouseover', changeColor);
+        }
+        etch.appendChild(row);
+    }
+};
+
+let clearBoard = () => {
+    etch.innerHTML = "";
+};
+
+
+etch.setAttribute('class', 'main');
+generateBoard(16);
 document.querySelector('body').appendChild(etch);
+
+
+
+
+
